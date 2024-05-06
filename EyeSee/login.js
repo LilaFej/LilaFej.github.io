85d91f9d5c657051918f680148c1a2142e2b3ef3
bhm900@myy.haaga-helia.fi <bhm900@myy.haaga-helia.fi>
2024-05-06 23:10:52 +0200

    h

--- EyeSee/login.js
+++ EyeSee/login.js
@@ -5,9 +5,12 @@ let indicator = document.getElementById("Indicator");
 function register(){
     regBox.style.transform = "translateX(0px)";
     loginBox.style.transform = "translateX(0px)";
+    indicator.style.transform = "translateX(100px)";
 }
 
 function logIn(){
     regBox.style.transform = "translateX(500px)";
     loginBox.style.transform = "translateX(500px)";
+    indicator.style.transform = "translateX(0px)";
+
 }
